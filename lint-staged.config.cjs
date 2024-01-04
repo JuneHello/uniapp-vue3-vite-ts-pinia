03aba40639b04b299c49d0d141531011f66706af
unknown <zhangfeng-119@gr.intra.cpic.com.cn>
2024-01-04 13:52:00 +0800

    feat: init

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "{!(package)*.json,*.code-snippets,.!(browserslist|nvm)*rc}": ["prettier --write--parser json"],
   "package.json": ["prettier --write"],
   "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
   "*.{scss,less,styl,html}": ["stylelint --fix", "prettier --write"],
